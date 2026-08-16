d59843fcdf8d2c79397f1dc71b2c749716f707f8
Ritvin R <166335211+Rr123454@users.noreply.github.com>
2026-08-16 10:43:13 -0700

    add LCD autonomous selector interface

---
+++ include/auton-selector.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace auton_selector {
+
+void initialize();
+void refresh();
+void previous();
+void next();
+void runSelected();
+const char* selectedName();
+
+} // namespace auton_selector
