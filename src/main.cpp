812b1dd3038235c0bbdd9e6018422e3ad635c34d
Rr123454 <ritvin.rejith@gmail.com>
2026-08-15 18:32:24 -0700

    C

--- src/main.cpp
+++ src/main.cpp
@@ -75,9 +75,9 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup left_mg({1, -2, 3});    // CCreates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
+	pros::MotorGroup lift_mg({7, 8});          // Creates a motor group with forwards ports 7 & 8
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
